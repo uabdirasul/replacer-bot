694852963913afc9c70a9104cba261ec675e60f1
uabdirasulm <ubbiniyazov04@gmail.com>
2024-08-30 17:35:14 +0500

    add recording interaction

--- bot.js
+++ bot.js
@@ -1,8 +1,11 @@
 const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
 require("dotenv").config();
 
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
 const token = process.env.TELEGRAM_BOT_TOKEN;
+const api_endpoint_post = process.env.API_ENDPOINT_POST;
+const bot_username = process.env.BOT_USERNAME;
 
 // Create a bot instance
 const bot = new TelegramBot(token, { polling: true });
@@ -45,6 +48,19 @@ bot.on("message", (msg) => {
       chatId,
       "Welcome! \nBul bot arqalı eski álipbedegi háriplerińizdi jańasına tez ózgertip alıwıńız múmkin. Onıń ushın tekstińizde ózgertiliwi kerek bolǵan hárip ' yáki ’ belgisi arqalı jazılıwı kerek. Mısalı: Sa'lem -> Sálem \n\nWebsayt: https://replaceletter.netlify.app/"
     );
+
+    axios
+      .post(`${api_endpoint_post}`, {
+        name: `${bot_username}`,
+        user_id: chatId,
+        interaction_time: getFormattedInteractionTime()
+      })
+      .then((response) => {
+        console.log("Interaction recorded:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error recording interaction:", error);
+      });
     // to stop further execution
     return;
   }
@@ -54,6 +70,19 @@ bot.on("message", (msg) => {
   // Send the formatted text back to the user
   bot.sendMessage(chatId, formattedText);
 });
+
+function getFormattedInteractionTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // zero-pad month
+  const day = String(now.getDate()).padStart(2, "0"); // zero-pad day
+  const hour = String(now.getHours()).padStart(2, "0");
+  const minute = String(now.getMinutes()).padStart(2, "0");
+  const second = String(now.getSeconds()).padStart(2, "0");
+  const millisecond = String(now.getMilliseconds()).padStart(6, "0"); // include milliseconds
+
+  return `${year}-${month}-${day} ${hour}:${minute}:${second}.${millisecond}`;
+}
 
 // Log any errors
 bot.on("polling_error", (error) => {
